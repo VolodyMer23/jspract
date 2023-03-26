b1346c302efcd519d54715a44fd69a7dd56b6f25
M-Farmaha <115003821+M-Farmaha@users.noreply.github.com>
2023-03-26 16:44:36 +0300

    lesson 6 task 3

--- js/lessons_6.js
+++ js/lessons_6.js
@@ -26,32 +26,62 @@
 //     }
 // }
 
-const form = document.querySelector("#form");
-const input = document.querySelector("#input");
-const wrapper = document.querySelector(".render");
+// const form = document.querySelector("#form");
+// const input = document.querySelector("#input");
+// const wrapper = document.querySelector(".render");
 
-form.addEventListener("submit", onSubmit);
+// form.addEventListener("submit", onSubmit);
 
-function onSubmit(event) {
-  event.preventDefault();
+// function onSubmit(event) {
+//   event.preventDefault();
 
-  const userValue = input.value;
+//   const userValue = input.value;
 
-  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userValue}`)
-    .then((responce) => responce.json())
-    .then((data) => sortRender(data.drinks))
-    .catch((err) => console.log(err));
-}
+//   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userValue}`)
+//     .then((responce) => responce.json())
+//     .then((data) => sortRender(data.drinks))
+//     .catch((err) => console.log(err));
+// }
 
-function render({ strDrink, strDrinkThumb, strCategory }) {
-  const murkUp = `<h2>${strDrink}</h2>
-  <img src="${strDrinkThumb}" alt="${strDrink}">
-  <p>${strCategory}</p>`;
-  wrapper.insertAdjacentHTML("beforeend", murkUp);
-}
+// function render({ strDrink, strDrinkThumb, strCategory }) {
+//   const murkUp = `<h2>${strDrink}</h2>
+//   <img src="${strDrinkThumb}" alt="${strDrink}">
+//   <p>${strCategory}</p>`;
+//   wrapper.insertAdjacentHTML("beforeend", murkUp);
+// }
 
-function sortRender(elements) {
+// function sortRender(elements) {
 
-    elements.forEach((element) => render(element));
-}
+//     elements.forEach((element) => render(element));
+// }
 
+// ЗАДАЧА 3
+// НАПИШІТЬ ПАГІНАЦІЮ, ДЛЯ ПЕРЕХОДУ ПО СТОРІНКАХ
+// https://docs.github.com/en/rest/search?apiVersion=2022-11-28#search-user
+// //
+// fetch(`https://api.github.com/search/users?q=${}&client_id=67684cabc84f94f0938e&client_secret=782ea639550c1b5d986bdd8129813652ed04c92c`)
+
+// const form = document.querySelector("#form");
+// const input = document.querySelector("#input");
+// const render = document.querySelector(".render");
+// const loadMoreBtn = document.querySelector(".load-more");
+// let count = 1;
+
+// form.addEventListener("submit", handleFormSubmit);
+// loadMoreBtn.addEventListener("click", handleFormSubmit);
+
+// function handleFormSubmit(e) {
+//   e.preventDefault();
+//   const inputValue = input.value;
+
+//   fetch(`https://api.github.com/search/users?q=${inputValue}&client_id=67684cabc84f94f0938e&client_secret=782ea639550c1b5d986bdd8129813652ed04c92c&per_page=5&page=${count}`)
+//     .then((data) => data.json())
+//     .then((data) => renderMarkup(data.items))
+//     .then(() => (count += 1))
+//     .catch((err) => console.log(err));
+// }
+
+// function renderMarkup(elements) {
+//   const elementMarkup = elements.map(({ login, avatar_url }) => `<h2>${login}</h2><img src="${avatar_url}" alt="${login}">`).join("");
+//   render.insertAdjacentHTML("beforeend", elementMarkup);
+// }
